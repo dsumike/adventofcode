b9d40ca5337b0e992c21b43002013335a1dfb592
Mike Johnson <dsumike@gmail.com>
2015-12-04 09:37:51 -0600

    Adding day 4 input and solutions

---
+++ python/04p2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import md5
+
+i = 1
+
+with open("../input/04.txt") as fileobj:
+	for word in fileobj:  
+		while True:
+			if md5.new("{}{}".format(word.rstrip(),i)).hexdigest()[:6] == '000000':
+				print word,i
+				break
+			i+=1
