cf166044320c6070a3b15d61deb50aff89622735
Mike Johnson <dsumike@gmail.com>
2015-12-15 09:53:14 -0600

    Adding day 10 input and solutions

---
+++ python/10p2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import re
+
+iterations = 40
+
+with open("../input/10.txt") as fileobj:
+	puzzle = fileobj.readline().strip()
+
+print puzzle
+
+for x in range(50):
+	newpuzzle = ''
+	for y in re.finditer(r"([0-9])\1*", puzzle):
+		newpuzzle += str(len(y.group(0)))+y.group(0)[0]
+	puzzle = newpuzzle
+
+print len(puzzle)
