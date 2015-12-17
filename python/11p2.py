ce587188bcfb54d38b6948ad448d22371bd29cdc
Mike Johnson <dsumike@gmail.com>
2015-12-17 10:10:40 -0600

    Adding day 11 input and solutions

---
+++ python/11p2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import re
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+with open("../input/11.txt") as fileobj:
+	password = fileobj.readline().strip()
+
+print password
+
+def rules(password):
+	rules = [ rule1, rule2, rule3 ]
+	if all(rule(password) for rule in rules):
+		return True
+	else:
+		return False
+
+def rule1(password):
+	# Rule 1: in the range of A-Z, must have 3 consecutive letters
+	# Check A-X for [abc, bcd, ..., wzy, xyz]
+	for i in range(24):
+		if letters[i:i+3] in password:
+			return True
+	# else rule 1 failed
+	return False
+
+def rule2(password):
+	# Rule 2: No i, o, l
+	if 'i' in password or 'o' in password or 'l' in password:
+		return False
+	return True
+
+def rule3(password):
+	# Rule 3: Password must contain at least 2 different, non-overlapping pairs of letters
+	# (aa, bb) or even (aa, aa) "aaaa"
+	pair = 0
+	skipnext = False
+	for i in range(len(password) - 1):
+		if skipnext:
+			skipnext = False
+			continue
+		if password[i] == password[i + 1]:
+			pair += 1
+			skipnext = True
+	return pair >1
+
+def increment(password):
+	try:
+		return password[0:-1] + letters[letters.index(password[-1]) + 1]
+	except IndexError:
+		return increment(password[0:-1]) + 'a'
+
+while True:
+	if rules(password):
+		print "Success! -- 1st password:", password
+		break
+	else:
+		password = increment(password)
+
+# Next run
+password = increment(password)
+
+while True:
+    if rules(password):
+        print "Success! -- 2nd password:", password
+        break
+    else:
+        password = increment(password)
